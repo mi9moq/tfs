1172416346aea2c4c3d481215d3ebd5cf9908059
Denis Mironov <denis5918@gmail.com>
2024-03-23 21:21:41 +0700

    добавил adapter

---
+++ app/src/main/java/com/mironov/coursework/ui/message/adapter/MainAdapter.kt
@@ -0,0 +1,27 @@
+package com.mironov.coursework.ui.message.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class MainAdapter : ListAdapter<DelegateItem, RecyclerView.ViewHolder>(
+    DelegateAdapterItemCallback()
+) {
+    private val delegates: MutableList<AdapterDelegate<DelegateItem>> = mutableListOf()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        delegates[viewType].onCreateViewHolder(parent)
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        delegates[getItemViewType(position)].onBindViewHolder(holder, getItem(position), position)
+    }
+
+    fun addDelegate(delegate: AdapterDelegate<DelegateItem>) {
+        delegates.add(delegate)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return delegates.indexOfFirst { it.isOfViewType(currentList[position]) }
+    }
+}
