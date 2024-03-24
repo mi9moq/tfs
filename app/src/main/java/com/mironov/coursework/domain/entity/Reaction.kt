74395e851825a49f4718e1584e831929cd721424
Denis Mironov <denis5918@gmail.com>
2024-03-24 14:28:20 +0700

    добавил проперти

--- app/src/main/java/com/mironov/coursework/domain/entity/Reaction.kt
+++ app/src/main/java/com/mironov/coursework/domain/entity/Reaction.kt
@@ -3,5 +3,6 @@ package com.mironov.coursework.domain.entity
 data class Reaction (
     val emojiUnicode: Int,
     val count: Int,
-    val userId: Int
+    val userId: Int,
+    val isSelected: Boolean = false
 )
