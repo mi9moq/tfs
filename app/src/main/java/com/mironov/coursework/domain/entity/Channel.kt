a4f7c7c75a5d318fd3ba04a5916a3ec7624e26ff
Denis Mironov <denis5918@gmail.com>
2024-04-01 20:58:18 +0700

    добавил сущности для каналов и топиков

---
+++ app/src/main/java/com/mironov/coursework/domain/entity/Channel.kt
@@ -0,0 +1,6 @@
+package com.mironov.coursework.domain.entity
+
+data class Channel(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/mironov/coursework/domain/entity/Topic.kt
@@ -0,0 +1,7 @@
+package com.mironov.coursework.domain.entity
+
+data class Topic(
+    val id: Int,
+    val name: String,
+    val messageCount: Int
+)
