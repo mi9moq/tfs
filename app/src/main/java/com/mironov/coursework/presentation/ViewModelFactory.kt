0da535e0c5fc77665ed347711d20de3757f76f4e
Denis Mironov <denis5918@gmail.com>
2024-04-02 14:55:49 +0700

    добавил фабрику view models

---
+++ app/src/main/java/com/mironov/coursework/presentation/ViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.mironov.coursework.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.mironov.coursework.di.AppScope
+import javax.inject.Inject
+import javax.inject.Provider
+
+@AppScope
+class ViewModelFactory @Inject constructor(
+    private val viewModelProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return viewModelProviders[modelClass]?.get() as T
+    }
+}
