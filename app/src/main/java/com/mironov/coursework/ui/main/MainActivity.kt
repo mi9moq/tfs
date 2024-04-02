bbcd5935d32a09c90d70ad0fe3af2b7bd0127305
Denis Mironov <denis5918@gmail.com>
2024-04-02 14:52:11 +0700

    рефактор

---
+++ app/src/main/java/com/mironov/coursework/ui/main/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.mironov.coursework.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.Router
+import com.github.terrakok.cicerone.androidx.AppNavigator
+import com.mironov.coursework.R
+import com.mironov.coursework.databinding.ActivityMainBinding
+import com.mironov.coursework.di.DaggerAppComponent
+import javax.inject.Inject
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    val component by lazy {
+        DaggerAppComponent.create()
+    }
+
+    @Inject
+    lateinit var router: Router
+
+    @Inject
+    lateinit var navigatorHolder: NavigatorHolder
+
+    private val navigator = AppNavigator(this, R.id.fragment_container)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        component.inject(this)
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navigatorHolder.removeNavigator()
+    }
+}
