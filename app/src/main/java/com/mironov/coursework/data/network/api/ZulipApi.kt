467ffcf0a9e6ef477b297ae253cb53151ff702ea
Denis Mironov <denis5918@gmail.com>
2024-04-13 14:48:49 +0700

    добавил сущности и методы для работы с юзерами

--- app/src/main/java/com/mironov/coursework/data/network/api/ZulipApi.kt
+++ app/src/main/java/com/mironov/coursework/data/network/api/ZulipApi.kt
@@ -3,6 +3,8 @@ package com.mironov.coursework.data.network.api
 import com.mironov.coursework.data.network.model.message.MessageResponse
 import com.mironov.coursework.data.network.model.streams.StreamResponse
 import com.mironov.coursework.data.network.model.streams.SubscribedStreamsResponse
+import com.mironov.coursework.data.network.user.AllUsersResponse
+import com.mironov.coursework.data.network.user.UserDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -26,4 +28,10 @@ interface ZulipApi {
         @Query("num_before") numBefore: Int = 150,
         @Query("num_after") numAfter: Int = 0,
     ): MessageResponse
+
+    @GET("users/me")
+    suspend fun getMyProfile(): UserDto
+
+    @GET("users")
+    suspend fun getAllUsersProfile(): AllUsersResponse
 }
---
+++ app/src/main/java/com/mironov/coursework/data/network/user/UserDto.kt
@@ -0,0 +1,12 @@
+package com.mironov.coursework.data.network.user
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserDto(
+    @SerialName("avatar_url") val avatarUrl: String?,
+    @SerialName("full_name") val userName: String,
+    @SerialName("email") val email: String,
+    @SerialName("user_id") val userId: Long
+)
