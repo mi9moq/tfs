403d72c6790054b01a649d85958da466e14f7092
Denis Mironov <denis5918@gmail.com>
2024-04-16 17:27:31 +0700

    вернул поиск на экране контактов

--- app/src/main/java/com/mironov/coursework/presentation/contacts/ContactsViewModel.kt
+++ app/src/main/java/com/mironov/coursework/presentation/contacts/ContactsViewModel.kt
@@ -6,9 +6,19 @@ import com.mironov.coursework.data.mapper.toEntity
 import com.mironov.coursework.data.network.api.ZulipApi
 import com.mironov.coursework.domain.entity.User
 import com.mironov.coursework.navigation.router.ContactsRouter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,9 +31,9 @@ class ContactsViewModel @Inject constructor(
     val state = _state.asStateFlow()
 
     val searchQuery = MutableSharedFlow<String>(extraBufferCapacity = 1)
+    private val cache = mutableListOf<User>()
 
     init {
-        //listenSearchQuery()
         viewModelScope.launch {
             _state.value = ContactsState.Loading
             val presences = api.getAllUserStatus().presences
@@ -31,20 +41,38 @@ class ContactsViewModel @Inject constructor(
                 val currentPresence = presences[it.email]?.toEntity() ?: User.Presence.OFFLINE
                 it.toEntity(currentPresence)
             }
+            cache.addAll(users)
             _state.value = ContactsState.Content(users)
         }
+        listenSearchQuery()
     }
 
-//
+    @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
+    private fun listenSearchQuery() {
+        searchQuery
+            .distinctUntilChanged()
+            .debounce(SEARCH_DURATION_MILLIS)
+            .mapLatest(::search)
+            .flowOn(Dispatchers.Default)
+            .onEach {
+                _state.value = it
+            }
+            .launchIn(viewModelScope)
+    }
 
-//    private suspend fun search(query: String): ContactsState {
-//        return viewModelScope.async {
-//            val newList = contactList.filter {
-//                it.userName.startsWith(query)
-//            }.toList()
-//            ContactsState.Content(newList)
-//        }.await()
-//    }
+
+    private suspend fun search(query: String): ContactsState {
+        return viewModelScope.async {
+            if (query.isBlank()){
+                ContactsState.Content(cache)
+            } else {
+                val newList = cache.filter {
+                    it.userName.startsWith(query)
+                }.toList()
+                ContactsState.Content(newList)
+            }
+        }.await()
+    }
 
     fun openProfile(userId: Int) {
         router.openProfile(userId)
