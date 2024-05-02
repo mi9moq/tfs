bac6540a81aba4e3e73ca5f5fc1edcaf1faaa503
Denis Mironov <denis5918@gmail.com>
2024-05-02 16:27:47 +0700

    добавил модели и dao для работы с топиками и стримами

---
+++ app/src/main/java/com/mironov/coursework/data/database/StreamDao.kt
@@ -0,0 +1,27 @@
+package com.mironov.coursework.data.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.mironov.coursework.data.database.model.StreamDbModel
+import com.mironov.coursework.data.database.model.TopicDbModel
+
+@Dao
+interface StreamDao {
+
+    @Query("SELECT * FROM ${StreamDbModel.TABLE_NAME}")
+    suspend fun getAllStreams(): List<StreamDbModel>
+
+    @Query("SELECT * FROM ${StreamDbModel.TABLE_NAME} WHERE is_subscribed = 1")
+    suspend fun getSubscribedStreams(): List<StreamDbModel>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStreams(streams: List<StreamDbModel>)
+
+    @Query("SELECT * FROM ${TopicDbModel.TABLE_NAME} WHERE stream_id = :streamId")
+    suspend fun getTopics(streamId: Int): List<TopicDbModel>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTopics(streams: List<TopicDbModel>)
+}
