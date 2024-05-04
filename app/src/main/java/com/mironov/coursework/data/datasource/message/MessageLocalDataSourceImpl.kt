5435d089eaba2245b133ba98f8865089fc463628
Denis Mironov <denis5918@gmail.com>
2024-05-05 01:10:20 +0700

    добавил local data source для сообщений

---
+++ app/src/main/java/com/mironov/coursework/data/datasource/message/MessageLocalDataSourceImpl.kt
@@ -0,0 +1,72 @@
+package com.mironov.coursework.data.datasource.message
+
+import com.mironov.coursework.data.database.MessageDao
+import com.mironov.coursework.data.database.model.message.MessageDbModel
+import javax.inject.Inject
+
+class MessageLocalDataSourceImpl @Inject constructor(
+    private val dao: MessageDao
+) : MessageLocalDataSource {
+
+    companion object {
+
+        private const val MESSAGE_CACHE_LIMIT = 50
+    }
+
+    override suspend fun getMessages(channelName: String, topicName: String): List<MessageDbModel> {
+        val messages = dao.getMessages(channelName, topicName)
+//        Log.e("MESSAGES", "size = ${messages.size}")
+//        messages.forEach {
+//            Log.e("MESSAGES", it.message.toString())
+//        }
+
+        return messages
+    }
+
+    override suspend fun insertMessages(
+        messages: List<MessageDbModel>,
+        channelName: String,
+        topicName: String,
+    ) {
+        dao.clearTopic(channelName, topicName)
+        dao.insertMessages(messages)
+    }
+
+    override suspend fun insertOldMessages(
+        messages: List<MessageDbModel>,
+        channelName: String,
+        topicName: String,
+    ) {
+        dao.insertMessages(messages)
+        val tableSize = dao.getTableSize()
+        if (tableSize <= MESSAGE_CACHE_LIMIT) return
+
+        val diff = tableSize - MESSAGE_CACHE_LIMIT
+        dao.deleteMessagesFromOtherTopics(channelName, topicName, diff)
+
+        val newSize = dao.getTableSize()
+        if (newSize <= MESSAGE_CACHE_LIMIT) return
+
+        val newDiff = newSize - MESSAGE_CACHE_LIMIT
+        dao.deleteNewMessages(newDiff)
+    }
+
+    override suspend fun insertNewMessages(
+        messages: List<MessageDbModel>,
+        channelName: String,
+        topicName: String,
+    ) {
+        dao.insertMessages(messages)
+        val tableSize = dao.getTableSize()
+        if (tableSize <= MESSAGE_CACHE_LIMIT) return
+
+        val diff = tableSize - MESSAGE_CACHE_LIMIT
+        dao.deleteMessagesFromOtherTopics(channelName, topicName, diff)
+
+        val newSize = dao.getTableSize()
+        if (newSize <= MESSAGE_CACHE_LIMIT) return
+
+        val newDiff = newSize - MESSAGE_CACHE_LIMIT
+        dao.deleteOldMessages(newDiff)
+    }
+}
